824130de81d5b0fca6748756d9bfe1ad1175873e
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-05-16 16:15:03 -0300

    fix: semaphores rearranged

--- Threads.java
+++ Threads.java
@@ -1,7 +1,10 @@
 import java.util.concurrent.Semaphore;
 
 public class Threads {
-    public static Semaphore Display = new Semaphore(1);
+    public static Semaphore Display = new Semaphore(0);
+    public static Semaphore Mutex = new Semaphore(1);
+    public static Semaphore Line = new Semaphore(0);
+    public static Semaphore IsWatching = new Semaphore(0);
     public static Semaphore EnterRoom = new Semaphore(5);  // semaforo para controlar o numero de pessoas dentro da sala para assistir, quando chega em 0, o fã não pode entrar
 
     static class Demonstrator extends Thread {
@@ -35,17 +38,14 @@ public class Threads {
         public void run(){
             while (true) {
 
-                if(EnterRoom.availablePermits() == 0){
-
-                    try {
-                        Display.acquire();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    displayMovie();
-                    Display.release();
-
+                try {
+                    Display.acquire();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+                IsWatching.a
+                displayMovie();
+
 
             }
         }
@@ -70,7 +70,7 @@ public class Threads {
 
 
         public void watchMovie(){
-            //entra na sala em fila e espera o displayMovie()
+            //espera o displayMovie()
 
         }
 
@@ -79,19 +79,71 @@ public class Threads {
 
         }
 
+        public void down(){
+            try {
+                    Mutex.acquire();
+                } 
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+        public void up(){
+            Mutex.release();
+        }
+
         public void run(){
 
             while (true) {
+                Line.release();
 
-                try{ 
+                // -----------------------------------------------
+                down();
+
+                try{
                     EnterRoom.acquire();
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                watchMovie();
+
+                try{ 
+                    Line.acquire();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                up();
+
+                // -----------------------------------------------
+                
+                down();
+
+                if(EnterRoom.availablePermits() == 0){
+                    Display.release();
+                }
+
+                up();
+                
+                try{ // usar esse mutex que começa em 0 e deixa todos dormindo ate que o demonstrador acordar e exibir o filme
+                    IsWatching.acquire();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                watchMovie(); // fazer algo que todos vejam o mesmo filme ao mesmo tempo quando Display.release()
+                
+                // -----------------------------------------------
+                
+                down();
+
                 EnterRoom.release();
                 
+                up();
+
+                // -----------------------------------------------
+
                 eat();
+
+                // -----------------------------------------------
 
             }
         }
